955084ee73c888385f8ab13ceece0429003b3e90
Dungeon Blueprint CI <>
2025-11-22 21:00:52 +0000

    Deploy new version of Dungeon Blueprint documentation

--- public/main.js
+++ public/main.js
@@ -1,11 +1,6 @@
 export default {
   iconLinks: [
     {
-      icon: 'github',
-      href: 'https://github.com/pws/dungeonblueprint',
-      title: 'GitHub'
-    },
-    {
       icon: 'discord',
       href: 'https://discord.gg/nwHcxHP2cT',
       title: 'Discord'
